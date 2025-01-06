eaabdaf7d2d0c1e86cd9965b055c75d0e895d366
varsha589 <varsha.deogharia44@gmail.com>
2025-01-06 09:10:49 +0530

    Step-2 added skeleton routes

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+app.listen(3000)
+
+app.post("/user/signup",()=>{
+
+})
+
+app.post("/user/signin",()=>{
+
+})
+
+app.get("/user/purchases",()=>{
+
+})
+
+app.get("/courses",()=>{
+
+})
+
+app.post("/course/purchase" , ()=>{
+
+})
